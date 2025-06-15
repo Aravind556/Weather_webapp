4fba530e068ebe6d606b7961a537053cd3e8dd55
Aravind556 <chintalapatiaravind30@gmail.com>
2025-06-15 15:36:32 +0100

    smh

--- src/main/java/com/example/Weather_forecast/controller/Content.java
+++ src/main/java/com/example/Weather_forecast/controller/Content.java
@@ -1,16 +1,14 @@
 package com.example.Weather_forecast.controller;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.result.view.RedirectView;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.result.view.RedirectView;
+
 
 
 @Controller
 public class Content {
 
-    @GetMapping("/page")
+    @GetMapping("/")
     public RedirectView welcome() {
         return new RedirectView("index.html");
     }
